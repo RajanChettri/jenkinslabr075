3626dd57e546fda9fcd532bf6f8a8e306e06009b
RajanChettri <rajan_chettri@live.com>
2024-08-01 11:15:27 +0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+
+    public static void main( String[] args) {
+
+        System.out.println("Author: Rajan");
+        System.out.println("Jenkins Project - Explaining Git Project Compilation");
+
+    }
+
+
+}
+
